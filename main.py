877294994cf55683f245198e96292c03cbe77d38
Unknown <p16097@students.cs.unipi.gr>
2019-07-03 15:28:33 +0300

    erotima 4 remains to be undirected - erotima 5 @40%

--- main.py
+++ main.py
@@ -5,43 +5,44 @@ import matplotlib.pyplot as plt
 
 
 def createPlots(num, graph):
-    nx.draw(graph)
-    # 4.1 Degree centrality plot
-    plt.hist(list(nx.degree_centrality(graph).values()))
-    plt.title('Degree Centrality')
-    plt.savefig('Degree%d' % num)
-    plt.clf()
-    # 4.2 In Degree centrality plot
-    plt.hist(list(nx.in_degree_centrality(graph).values()))
-    plt.title('In Degree Centrality')
-    plt.savefig('InDegree%d' % num)
-    plt.clf()
-    # 4.3 Out Degree centrality plot
-    plt.hist(list(nx.out_degree_centrality(graph).values()))
-    plt.title('Out Degree Centrality')
-    plt.savefig('OutDegree%d' % num)
-    plt.clf()
-    # 4.4 Closeness centrality plot
-    plt.hist(list(nx.closeness_centrality(graph).values()))
-    plt.title('Closeness Centrality')
-    plt.savefig('Closeness%d' % num)
-    plt.clf()
-    # 4.5 Betweeness centrality plot
-    plt.hist(list(nx.betweenness_centrality(graph).values()))
-    plt.title('Betweeness Centrality')
-    plt.savefig('Betweeness%d' % num)
-    plt.clf()
-    # 4.6 Eigenvector centrality plot
-    plt.hist(list(nx.eigenvector_centrality_numpy(graph).values()))
-    plt.title('Eigenvector Centrality')
-    plt.savefig('Eigenvector%d' % num)
-    plt.clf()
-    # 4.7 Katz centrality plot
-    plt.hist(list(nx.katz_centrality(G).values()))
-    plt.title('Katz Centrality')
-    plt.savefig('Katz%d' % num)
-    plt.clf()
-    print("All centralities have been plotted and saved !!")
+    if G != set():
+        nx.draw(graph)
+        # 4.1 Degree centrality plot
+        plt.hist(list(nx.degree_centrality(graph).values()))
+        plt.title('Degree Centrality')
+        plt.savefig('Degree%d' % num)
+        plt.clf()
+        # 4.2 Degree centrality plot
+        plt.hist(list(nx.in_degree_centrality(graph).values()))
+        plt.title('In Degree Centrality')
+        plt.savefig('InDegree%d' % num)
+        plt.clf()
+        # 4.3 Degree centrality plot
+        plt.hist(list(nx.out_degree_centrality(graph).values()))
+        plt.title('Out Degree Centrality')
+        plt.savefig('OutDegree%d' % num)
+        plt.clf()
+        # 4.4 Degree centrality plot
+        plt.hist(list(nx.closeness_centrality(graph).values()))
+        plt.title('Closeness Centrality')
+        plt.savefig('Closeness%d' % num)
+        plt.clf()
+        # 4.5 Degree centrality plot
+        plt.hist(list(nx.betweenness_centrality(graph).values()))
+        plt.title('Betweeness Centrality')
+        plt.savefig('Betweeness%d' % num)
+        plt.clf()
+        # 4.6 Degree centrality plot
+        plt.hist(list(nx.eigenvector_centrality_numpy(graph).values()))
+        plt.title('Eigenvector Centrality')
+        plt.savefig('Eigenvector%d' % num)
+        plt.clf()
+        # 4.7 Degree centrality plot
+        plt.hist(list(nx.katz_centrality(graph).values()))
+        plt.title('Katz Centrality')
+        plt.savefig('Katz%d' % j)
+        plt.clf()
+        print("All centralities have been plotted and saved !!")
 
 
 filepath = './sx-stackoverflow.txt'
@@ -87,23 +88,53 @@ print()
 # The list of time dependent graphs
 graphs = [nx.Graph() for _ in range(len(T_list))]
 
+num = 0
 for _, row in df.iterrows():
     source_id = row['source_id']
     target_id = row['target_id']
     timestamp = row['timestamp']
-
+    G = nx.DiGraph()  # empty graph
     for idx, period in enumerate(T_list):
         min = period[0]
         max = period[1]
-        G = nx.DiGraph() #empty graph
+
         if (idx == len(T_list) - 1 and min <= timestamp <= max) or min <= timestamp < max:
             G = graphs[idx]
             G.add_node(source_id)
             G.add_node(target_id)
             G.add_edge(source_id, target_id)
             break
-        G.remove_edges_from(G.selfloop_edges()) #no loops over itself
-        createPlots(row, G)  #erotima 4
+
+
+    # erotima 4
+    print()
+    print('4o erotima')
+    print()
+    G.remove_edges_from(G.selfloop_edges())
+    if G != nx.empty_graph:
+        createPlots(num, G)
+
+    # erotima 5
+    nodes = G.nodes
+    edges = G.edges
+
+    Vs = []
+    Es = []
+    for i in range(num):
+        try:
+            tmp = set(nodes[i + 1]) - set(nodes[i])
+            tmp2 = set(edges[i + 1]) - set(edges[i])
+            Vs.append(set(nodes[i]) - set(tmp))
+            Es.append(set(nodes[i]) - set(tmp2))
+            continue
+        except:
+            break
+    print("V star")
+    print(Vs)
+    print()
+    print("E star")
+    print(Es)
+    num += 1
 
 for graph in graphs:
     nx.draw(graph, **{
