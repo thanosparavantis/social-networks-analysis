58d96473be3fb26261bae4c56e82179c63202787
Unknown <p16097@students.cs.unipi.gr>
2019-07-03 14:47:07 +0300

    erotima 4

--- main.py
+++ main.py
@@ -3,6 +3,47 @@ import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+def createPlots(num, graph):
+    nx.draw(graph)
+    # 4.1 Degree centrality plot
+    plt.hist(list(nx.degree_centrality(graph).values()))
+    plt.title('Degree Centrality')
+    plt.savefig('Degree%d' % num)
+    plt.clf()
+    # 4.2 In Degree centrality plot
+    plt.hist(list(nx.in_degree_centrality(graph).values()))
+    plt.title('In Degree Centrality')
+    plt.savefig('InDegree%d' % num)
+    plt.clf()
+    # 4.3 Out Degree centrality plot
+    plt.hist(list(nx.out_degree_centrality(graph).values()))
+    plt.title('Out Degree Centrality')
+    plt.savefig('OutDegree%d' % num)
+    plt.clf()
+    # 4.4 Closeness centrality plot
+    plt.hist(list(nx.closeness_centrality(graph).values()))
+    plt.title('Closeness Centrality')
+    plt.savefig('Closeness%d' % num)
+    plt.clf()
+    # 4.5 Betweeness centrality plot
+    plt.hist(list(nx.betweenness_centrality(graph).values()))
+    plt.title('Betweeness Centrality')
+    plt.savefig('Betweeness%d' % num)
+    plt.clf()
+    # 4.6 Eigenvector centrality plot
+    plt.hist(list(nx.eigenvector_centrality_numpy(graph).values()))
+    plt.title('Eigenvector Centrality')
+    plt.savefig('Eigenvector%d' % num)
+    plt.clf()
+    # 4.7 Katz centrality plot
+    plt.hist(list(nx.katz_centrality(G).values()))
+    plt.title('Katz Centrality')
+    plt.savefig('Katz%d' % num)
+    plt.clf()
+    print("All centralities have been plotted and saved !!")
+
+
 filepath = './sx-stackoverflow.txt'
 
 print('StackOverflow Dataset')
@@ -54,13 +95,15 @@ for _, row in df.iterrows():
     for idx, period in enumerate(T_list):
         min = period[0]
         max = period[1]
-
+        G = nx.DiGraph() #empty graph
         if (idx == len(T_list) - 1 and min <= timestamp <= max) or min <= timestamp < max:
             G = graphs[idx]
             G.add_node(source_id)
             G.add_node(target_id)
             G.add_edge(source_id, target_id)
             break
+        G.remove_edges_from(G.selfloop_edges()) #no loops over itself
+        createPlots(row, G)  #erotima 4
 
 for graph in graphs:
     nx.draw(graph, **{
